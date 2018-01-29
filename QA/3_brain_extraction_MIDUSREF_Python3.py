305ef3f24b056a89ddef12f7f86187cef2a71582
Davids <neuro.dlee@gmail.com>
2018-01-29 13:00:54 -0600

    deleted unncessary comments

--- QA/3_brain_extraction_MIDUSREF_Python3.py
+++ QA/3_brain_extraction_MIDUSREF_Python3.py
@@ -63,22 +63,5 @@ for sub in subPath:
 		#os.system("bet %s/%s_T1w_2.nii.gz %s/%s/anat/%s_T1w_2_0.3_brain.nii.gz -R -m"%(sub, subNumFull, analysis_path, subNumFull, subNumFull))
 		os.system("bet %s/%s_T1w_2.nii.gz %s/%s/anat/%s_T1w_2_0.3_brain.nii.gz -m -f 0.3 -R"%(sub, subNumFull, analysis_path, subNumFull, subNumFull))
 
-### Jeanette's comments ###
 
-# If you want to try out freesurfer, here's the command line code that
-# you can adapt to the loop via os.system.  Mostly, you'll need to put actual paths in.
-
-# I think it needs unzipped files (double check this)
-# gunzip path/to/anatomy/highres001.nii.gz
-
-# This takes a while (~15 minutes?)
-# recon-all -autorecon1 -i path/to/anatomy/highres001.nii -subjid autorecon   -sd /path/to/anatomy/
-# This will actually create the skull stripped brain (you won't get a mask)
-
-#mri_convert  /path/to/anatomy/autorecon/mri/brainmask.mgz  --reslice_like /path/to/anatomy/highres001.nii /path/to/anatomy/highres001_brain.nii
-
-# I'm deleting the files it created
-#rm -rf /path/to/anatomy/autorecon/
-
-# zipping up the skull stripped image and original image
 #gzip /path/to/anatomy/*.nii
