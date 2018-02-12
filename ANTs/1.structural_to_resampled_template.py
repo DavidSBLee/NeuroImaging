52428cd09964404b1d1cf8397ac4ab883113a6cd
Davids <neuro.dlee@gmail.com>
2018-02-12 14:08:14 -0600

    update script

--- ANTs/1.structural_to_resampled_template.py
+++ ANTs/1.structural_to_resampled_template.py
@@ -8,7 +8,6 @@ print(sys.argv)
 subNum = sys.argv[1]
 
 path = "/study4/midusref/DATA/mri/processed/david/%s"%(subNum)
-#brain = glob.glob("%s/%s_T1w_fslanat_brain.nii.gz"%(path, subNum))
 
 #os.mkdir("/study4/midusref/DATA/mri/processed/david/%s"%(subNum))
-subprocess.call(["antsRegistrationSyN.sh", "-d", "3", "-f", "/home/slee/Desktop/xMAYO_T/template_brain_MIDUS3_resampled.nii.gz", "-m", "%s/%s_T1w_fslanat_brain.nii.gz"%(path, subNum), "-o", "/study4/midusref/DATA/mri/processed/david/%s/%s_structural_to_resampled_template_"%(subNum,subNum), "-t", "s"])
+subprocess.call(["antsRegistrationSyN.sh", "-d", "3", "-f", "/path/to/template", "-m", "%s/%s_T1w_fslanat_brain.nii.gz"%(path, subNum), "-o", "/study4/midusref/DATA/mri/processed/david/%s/%s_structural_to_resampled_template_"%(subNum,subNum), "-t", "s"])
