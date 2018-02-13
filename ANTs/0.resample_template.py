91f2e697832c066336d908a51c683f2f31b7bbd8
David Lee <sleedave90@gmail.com>
2018-02-13 09:59:40 -0600

    Update Content

--- ANTs/0.resample_template.py
+++ ANTs/0.resample_template.py
@@ -1,7 +1,15 @@
- #!/usr/bin/env python3
+#!/bin/python3
+
+# Author: David SB Lee
+# First Compiled: 12/12/2017
+# Purpose: Resample a nifti image with a new voxel dimension
+
+# Things to change when running on different data set
+# 1. All Paths
 
 import os
- # Resample a template image with 1x1x1 voxel dimension
- # Change the voxel dimension of your choice
- # The change in the voxel dimension will change the number of total dimensions
+
+# Resample a template image with 1x1x1 voxel dimension
+# Change the voxel dimension of your choice
+# The change in the voxel dimension will change the number of total dimensions
 os.system("ResampleImage 3 /path/to/template /path/to/resampled/template 1x1x1")
