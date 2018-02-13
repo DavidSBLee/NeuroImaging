02100ea2c5f50e6c6d10525cd2bd4c7107a55e1a
David Lee <sleedave90@gmail.com>
2018-02-13 09:58:10 -0600

    Update Content

--- ANTs/1.structural_to_resampled_template.py
+++ ANTs/1.structural_to_resampled_template.py
@@ -1,4 +1,13 @@
- #!/usr/bin/env python3
+#!/bin/python3
+
+# Author: David SB Lee
+# First Compiled: 12/13/2017
+# Purpose: Generate registration calculations for SyN (Type of Nonlinear Registration: Symmetric Normalization)
+# SyN Based Warping == Linear + Non-Linear bidirectional mapping
+
+# Things to change when running on different data set
+# 1. All Paths
+
 import subprocess
 import glob
 import sys
@@ -7,7 +16,17 @@ import os
 print(sys.argv)
 subNum = sys.argv[1]
 
-path = "/study4/midusref/DATA/mri/processed/david/%s"%(subNum)
+path = "/path/to/subdir"
 
-#os.mkdir("/study4/midusref/DATA/mri/processed/david/%s"%(subNum))
-subprocess.call(["antsRegistrationSyN.sh", "-d", "3", "-f", "/path/to/template", "-m", "%s/%s_T1w_fslanat_brain.nii.gz"%(path, subNum), "-o", "/study4/midusref/DATA/mri/processed/david/%s/%s_structural_to_resampled_template_"%(subNum,subNum), "-t", "s"])
+subprocess.call(["antsRegistrationSyN.sh", "-d", "3", "-f", "/path/to/template/nifti", 
+                 "-m", "/path/to/template/nifti", 
+                 "-o", "/path/to/output/nifti, "-t", "s"])
+
+### Example
+#path = "/study4/midusref/DATA/mri/processed/david/Post_Anonymization/%s/"%(subNum)
+#subprocess.call(["antsRegistrationSyN.sh", "-d", "3", "-f", 
+#                "/study4/midusref/DATA/mri/processed/david/scripts/Mayo_T1w_resampled_template.nii.gz", 
+#                "-m", "%s/%s_T1w.nii.gz"%(path, subNum), 
+#                "-o", "/study4/midusref/DATA/mri/processed/david/Post_Anonymization/%s/%s_structural_to_resampled_template_"%(subNum,subNum), "-t", "s"])
+                 
+                 
