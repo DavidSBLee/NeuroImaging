0db014537e7e6939bbc53cdddae51128fe3f45d3
Seung Beum Lee <SB@Seungui-MacBook-Pro.local>
2018-12-02 14:37:42 -0600

    delete scripts

--- ANTs/4.change_pixdim.py
+++
@@ -1,4 +0,0 @@
-
-
-nifti_tool -mod_hdr -prefix test_epi_edited.nii -infiles test_epi.nii -mod_field pixdim '1.0 4.0 1.7188 1.7188 DESIREDTR 0.0 0.0 0.0'
-nifti_tool -mod_hdr -prefix test_epi_edited.nii -infiles sub-001_normalized.nii.gz -mod_field pixdim '1.0 4.0 1.7188 1.7188 1.0 0.0 0.0 0.0'
